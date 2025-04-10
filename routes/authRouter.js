78384daeae7318f09f95693c690bf7bef761b42b
blazejzj <opexrrator@gmail.com>
2025-04-10 22:12:40 +0200

    styled header and dashboard

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -8,6 +8,6 @@ authRouter.post("/register", authController.register);
 authRouter.get("/login", authController.showLoginForm);
 authRouter.post("/login", authController.login);
 
-authRouter.post("/logout", authController.logout);
+authRouter.get("/logout", authController.logout);
 
 module.exports = authRouter;
