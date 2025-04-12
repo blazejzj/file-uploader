ec580d33a648e29812f0060458f4b87027f618ae
blazejzj <opexrrator@gmail.com>
2025-04-12 13:13:33 +0200

    refactor: slight more cleanup, added check before logging out

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const authRouter = express.Router();
 const authController = require("../controllers/authController");
-const { isGuest } = require("../middleware/authMiddleware");
+const { isGuest, isAuthenticated } = require("../middleware/authMiddleware");
 
 authRouter.get("/register", isGuest, authController.showRegisterForm);
 authRouter.post("/register", isGuest, authController.register);
@@ -9,6 +9,6 @@ authRouter.post("/register", isGuest, authController.register);
 authRouter.get("/login", isGuest, authController.showLoginForm);
 authRouter.post("/login", isGuest, authController.login);
 
-authRouter.get("/logout", authController.logout);
+authRouter.get("/logout", isAuthenticated, authController.logout);
 
 module.exports = authRouter;
